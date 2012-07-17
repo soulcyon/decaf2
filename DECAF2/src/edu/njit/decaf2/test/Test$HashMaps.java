10e941aaff6eaa192e36d7c098ee4c1e80badb94
Sashank%20Tadepalli <soulcyon@gmail.com>
2012-07-17 13:14:47 -0400

    Initial Commit

---
+++ DECAF2/src/edu/njit/decaf2/test/Test$HashMaps.java
@@ -0,0 +1,49 @@
+package edu.njit.decaf2.test;
+
+import java.util.HashMap;
+
+import edu.njit.decaf2.data.FailureNode;
+
+/**
+ * @author soulcyon
+ *
+ */
+public class Test$HashMaps {
+	public static void main(String[] args){
+		/****************************************************************************/
+		/* FailureNode-Double HashMap Benchmark */
+		/****************************************************************************/
+		
+		HashMap<FailureNode, double[]> test = new HashMap<FailureNode, double[]>();
+		int i = 0;
+		
+		// Java heap space issues > 3,500,000 elements
+		int max = 1000000;
+		
+		double t = System.nanoTime();
+		while( i++ < max ){
+			double[] temp = new double[]{Math.random()};
+			test.put(new FailureNode("A", temp), temp);
+		}
+		System.out.println("Pushing " + max + " (" + test.size() + ") Components to HashMap.");
+		System.out.println((System.nanoTime() - t)/1000.0/1000.0/1000.0);
+		
+		/****************************************************************************/
+		/* Double-Double HashMap Benchmark */
+		/****************************************************************************/
+		
+		HashMap<Double, Double> test2 = new HashMap<Double, Double>();
+		i = 0;
+		
+		// Java heap space issues > 20,000,000 elements
+		max = 500000;
+		
+		t = System.nanoTime();
+		while( i++ < max ){
+			double temp = Math.random();
+			test2.put(temp, temp);
+		}
+		System.out.println("Pushing " + max + " (" + test2.size() + ") Doubles to HashMap.");
+		System.out.println((System.nanoTime() - t)/1000.0/1000.0/1000.0);
+	}
+}
