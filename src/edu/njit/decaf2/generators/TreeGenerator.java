8bedf1109ff80e8b5d3f8d0f87b9442174a22abf
Sashank%20Tadepalli <soulcyon@gmail.com>
2012-07-19 01:14:50 -0400

    Initial TreeGen Code
    Added wrapper code for Tree Generation

--- src/edu/njit/decaf2/generators/TreeGenerator.java
+++ src/edu/njit/decaf2/generators/TreeGenerator.java
@@ -1,5 +1,12 @@
 package edu.njit.decaf2.generators;
 
+import java.util.HashMap;
+
+import edu.njit.decaf2.DECAF;
+import edu.njit.decaf2.data.FailureNode;
+import edu.njit.decaf2.data.FailureTree;
+import edu.njit.decaf2.data.State;
+
 /**
  * ______  _______ _______ _______ _______      _____ _____
  * |     \ |______ |       |_____| |______        |     |  
@@ -8,6 +15,51 @@ package edu.njit.decaf2.generators;
  * @author Sashank Tadepalli
  *
  */
-public class TreeGenerator {
+public class TreeGenerator extends DECAF {
+	private HashMap<String, FailureTree> 	cache = new HashMap<String, FailureTree>();
+	private HashMap<String, FailureNode>	nodeM = new HashMap<String, FailureNode>();
+	private int								misses = 0;
+	
+	/**
+	 * @param decaf_nodeMap
+	 */
+	public TreeGenerator(HashMap<String, FailureNode> nodeM) {
+		this.nodeM = nodeM;
+	}
 
+	/**
+	 * 
+	 * @param from
+	 * @param to
+	 * @return
+	 */
+	public double getFailureRate(State from, State to){
+		if( cache.containsKey(from.diff(to) + "") ){
+			misses++;
+		} else {
+			cache.put(from.diff(to) + "", new FailureTree(nodeM.get(0)));
+		}
+		return -1.0;
+	}
+
+	/**
+	 * @return the nodeM
+	 */
+	public HashMap<String, FailureNode> getNodeM() {
+		return nodeM;
+	}
+	
+	/**
+	 * @return the cache
+	 */
+	public HashMap<String, FailureTree> getCache() {
+		return cache;
+	}
+
+	/**
+	 * @return the misses
+	 */
+	public int getMisses() {
+		return misses;
+	}
 }
