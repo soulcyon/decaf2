b19941f723a99895008330e40e39db46da0e7217
Sashank%20Tadepalli <soulcyon@gmail.com>
2012-09-07 17:06:05 -0400

    Updated QMatrix logic
    Skipping diagonals isn't so complicated :D

--- src/edu/njit/decaf2/generators/QMatrixGeneratorUnthreaded.java
+++ src/edu/njit/decaf2/generators/QMatrixGeneratorUnthreaded.java
@@ -53,10 +53,8 @@ public class QMatrixGeneratorUnthreaded extends DECAF {
 
 		// Iterate over matrix, ignore diagonal
 		for (int i = 0; i < statesLen; i++) {
-			for (int j = i == 0 ? 1 : 0; j < statesLen; j = j == i - 1 ? j + 2 : j + 1) {
+			for (int j = i + 1; j < statesLen; j++) {
 
-				if (j == i)
-					continue;
 				double fillV = fillQMatrix(Simulation.states[i], Simulation.states[j]);
 
 				if (Double.isNaN(fillV)) {
