e17d75305ed6400641bd602e8ec4465bf1ed91b5
Sashank Tadepalli <soulcyon@gmail.com>
2012-09-26 23:50:10 -0400

    Parameter modifications

--- src/edu/njit/decaf2/DECAF.java
+++ src/edu/njit/decaf2/DECAF.java
@@ -16,16 +16,16 @@ import java.util.concurrent.ForkJoinPool;
  */
 public class DECAF {
 	protected static final boolean sriniOutput = false;
-	protected static final boolean forceStateDemandValidate = false;
+	protected static final boolean forceStateDemandValidate = true;
 	protected static final boolean verboseDebug = false;
 	protected static final int debugX = 0;
 	protected static final int debugY = 8;
 	protected static final boolean enableThreading = false;
-	public static final int threadCount = 8;
+	public static final int threadCount = 4;
 	public static final ForkJoinPool threadPool = new ForkJoinPool(256);
-	public static final int heightThreshold = 6;
-	public static final double rateThreshold = 0.0005;
-	public static final int nodeThreshold = 8; 
+	public static final int heightThreshold = 100;
+	public static final double rateThreshold = 0;
+	public static final int nodeThreshold = 100; 
 	public static final String biasType = "high";
 
 	/**
